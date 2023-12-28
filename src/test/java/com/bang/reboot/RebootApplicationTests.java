826c7110fba55847c1bf0085e9264d8b6189bf5d
banghyuk <bangdong777@gamil.com>
2023-12-28 15:45:01 +0900

    환경설정세팅

---
+++ src/test/java/com/bang/reboot/RebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bang.reboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
